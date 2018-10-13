3df01058f0626686a2e970812a91e9e549e6b70a
Francesco Pirotti <francesco.pirotti@unipd.it>
2018-10-13 20:32:07 +0200

    Update lfx-canvas-markers.js

--- inst/htmlwidgets/bindings/lfx-canvas-markers.js
+++ inst/htmlwidgets/bindings/lfx-canvas-markers.js
@@ -1,1 +1,12 @@
 
+/* global LeafletWidget, L */
+
+LeafletWidget.methods.addCanvasMarkers = function(lat, lng) {
+  (
+    function() {
+      L.CanvasIconLayer(
+        [lat, lng]
+      ).addTo(this);
+    }
+  ).call(this);
+};
